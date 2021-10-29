649f477263b982d48e6b0e181626c5be4029fb64
dequeues <dequeues@outlook.com>
2021-10-29 19:56:38 +0100

    More updates

--- gulpfile.js
+++ gulpfile.js
@@ -3,23 +3,41 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass')(require('sass'));
 var header = require('gulp-header');
+const rename = require('gulp-rename');
 
 const headerTemplate = `/* ==UserStyle==
 @name           reddit toolbox dark userstyle
 @namespace      https://github.com/dequeues/Reddit-Toolbox-Dark-Userstyle
-@version        1.0.0
+@version        ${require("./package.json").version}
 @description    Dark mode for reddit toolbox on new reddit
 @author         dequeues
 @supportURL     https://github.com/dequeues/Reddit-Toolbox-Dark-Userstyle/issues
-@updateURL      https://gist.githubusercontent.com/dequeues/d8a6f21637da75fe2bbd057ac2d2a3e7/raw/ec0b768f83ebfdfbd38040dc87a2bfe27641d89d/lmao.user.css
+@updateURL      https://github.com/dequeues/Reddit-Toolbox-Dark-Userstyle/raw/main/dist/toolbox-dark.user.css
 ==/UserStyle== */`;
 
 
-function buildStyles() {
+function buildStylus() {
   return gulp.src('**/*.scss')
     .pipe(sass().on('error', sass.logError))
     .pipe(header(`${headerTemplate}\n\n`))
+    .pipe(rename((path) => {
+        path.dirname = "";
+        path.basename = "toolbox-dark",
+        path.extname = ".user.css"
+    }))
     .pipe(gulp.dest('./dist'));
 };
 
-gulp.task('css', buildStyles);
+function buildRawStylesheet() {
+    return gulp.src('**/*.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(header(`${headerTemplate}\n\n`))
+    .pipe(rename((path) => {
+        path.dirname = "";
+        path.basename = "toolbox-dark",
+        path.extname = ".css"
+    }))
+    .pipe(gulp.dest('./dist'));
+}
+
+exports.release = gulp.series(buildStylus, buildRawStylesheet);
